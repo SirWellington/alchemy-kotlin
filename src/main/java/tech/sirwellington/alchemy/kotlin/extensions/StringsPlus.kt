71c9d70965e244172f9cfdb0bacb3a9f18c5a21f
Wellington Moreno <wellington@censio.love>
2017-11-29 20:42:46 -0800

    Adds String extension functions

--- src/main/java/tech/sirwellington/alchemy/kotlin/extensions/StringsPlus.kt
+++ src/main/java/tech/sirwellington/alchemy/kotlin/extensions/StringsPlus.kt
@@ -1,8 +1,77 @@
+/*
+ * Copyright 2017 SirWellington Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.sirwellington.alchemy.kotlin.extensions
+
+
 /**
  *
  * @author SirWellington
  */
-package tech.sirwellington.alchemy.kotlin.extensions
 
+fun String.titleCased(): String
+{
+    if (isEmpty()) return this
 
-private object StringsPlus
+    val firstCharacter = this[0].toString()
+    val firstCharacterCapitalized = firstCharacter.toUpperCase()
+
+    if (length == 1) return firstCharacterCapitalized
+
+    val otherCharacters = substring(1 until length)
+    val otherCharactersLowerCased = otherCharacters.toLowerCase()
+
+    return firstCharacterCapitalized + otherCharactersLowerCased
+}
+
+/**
+ * Variable version of [String.isEmpty]
+ */
+val String.isEmpty get() = isEmpty()
+
+/**
+ * Convenient variable to check whether the given
+ * string is empty or null.
+ */
+val String?.isEmptyOrNull: Boolean
+    get()
+    {
+        if (this == null) return true
+
+        return isEmpty()
+    }
+
+/**
+ * Returns true, unless the String is empty or null.
+ * This is a useful check to see whether a string is good to
+ * use or not.
+ */
+val String?.notEmptyOrNull: Boolean get() = !this.isEmptyOrNull
+
+/**
+ * Gets the first letter of the given string,
+ * unless the string is empty, in which case the first letter will be null.
+ *
+ * @return The first letter of this string, or `null` if this string is empty.
+ */
+val String.firstLetter: String?
+    get()
+    {
+        if (isEmpty) return null
+
+        val firstCharacter = this[0]
+        return firstCharacter.toString()
+    }
---
+++ src/test/java/tech/sirwellington/alchemy/kotlin/extensions/StringsPlusKtTest.kt
@@ -0,0 +1,35 @@
+package tech.sirwellington.alchemy.kotlin.extensions
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner
+
+@RunWith(AlchemyTestRunner::class)
+class StringsPlusKtTest
+{
+    @Test
+    fun testTitleCased()
+    {
+    }
+
+    @Test
+    fun testIsEmpty()
+    {
+    }
+
+    @Test
+    fun testIsEmptyOrNull()
+    {
+    }
+
+    @Test
+    fun testGetNotEmptyOrNull()
+    {
+    }
+
+    @Test
+    fun testGetFirstLetter()
+    {
+    }
+
+}
