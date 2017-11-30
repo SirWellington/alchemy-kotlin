88b082820ff99aa40bfeecca4180d80528ba8ae6
Wellington Moreno <wellington@censio.love>
2017-11-29 20:42:34 -0800

    Adds Array extension functions

---
+++ src/main/java/tech/sirwellington/alchemy/kotlin/extensions/ArraysPlus.kt
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2017 SirWellington Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.sirwellington.alchemy.kotlin.extensions
+
+
+/**
+ *
+ * @author SirWellington
+ */
+
+/**
+ * Checks whether [index] is a valid index in `this`
+ * array.
+ *
+ * @return `true` if [index] is a valid index in this array, false otherwise.
+ * @see [notValidIndex]
+ */
+fun <T> Array<T>.isValidIndex(index: Int): Boolean
+{
+    if (index < 0) return false
+
+    return index < size
+}
+
+/**
+ * Reversed condition of [isValidIndex].
+ * This function checks to make sure [index] is an invalid index in `this` array.
+ *
+ * @return `true` if [index] is not a valid index in `this` array, `false` if it is.
+ */
+fun <T> Array<T>.notValidIndex(index: Int) = !isValidIndex(index)
+
+
+/**
+ * Checks whether `this` [Int] is a valid index in [array].
+ *
+ * @return `true` if `this` Int is a valid index in [array], `false, otherwise.
+ */
+fun <T> Int.isValidIndexIn(array: Array<T>): Boolean
+{
+    if (this < 0) return false
+
+    return this < array.size
+}
+
+/**
+ * A reverse condition of [isValidIndexIn]
+ * Checks whether `this` [Int] is an invalid index in [array].
+ *
+ * @return `true` if `this` is an invalid index in [array], false if it is valid.
+ * @see [notValidIndex]
+ * @see [isValidIndexIn]
+ */
+fun <T> Int.notValidIndexIn(array: Array<T>) = !isValidIndexIn(array)
+
+/**
+ * Access a random element in this array.
+ *
+ * @return A random element in this array, `null` if the array is empty.
+ */
+val <T> Array<T>.anyElement: T?
+    get()
+    {
+        if (isEmpty()) return null
+
+        val range = (0 until size)
+        val index = range.random()
+
+        return this.getOrNull(index)
+    }
---
+++ src/test/java/tech/sirwellington/alchemy/kotlin/extensions/ArraysPlusKtTest.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2017 SirWellington Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.sirwellington.alchemy.kotlin.extensions
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.isIn
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import tech.sirwellington.alchemy.generator.CollectionGenerators
+import tech.sirwellington.alchemy.generator.NumberGenerators
+import tech.sirwellington.alchemy.generator.StringGenerators
+import tech.sirwellington.alchemy.generator.one
+import tech.sirwellington.alchemy.test.hamcrest.isNull
+import tech.sirwellington.alchemy.test.hamcrest.notNull
+
+class ArraysPlusKtTest
+{
+
+    private lateinit var array: Array<String>
+
+    private var negativeIndex = -1
+    private var outOfBoundsIndex = 0
+    private var goodIndex = 0
+
+    @Before
+    fun setUp()
+    {
+        array = CollectionGenerators.listOf(StringGenerators.strings()).toTypedArray()
+
+        negativeIndex = one(NumberGenerators.negativeIntegers())
+        outOfBoundsIndex = one(NumberGenerators.integers(array.size, Int.MAX_VALUE))
+        goodIndex = one(NumberGenerators.integers(0, array.size))
+    }
+
+    @Test
+    fun isValidIndexWhenValid()
+    {
+        assertTrue(array.isValidIndex(0))
+        assertTrue(array.isValidIndex(goodIndex))
+    }
+
+    @Test
+    fun isValidIndexWhenInvalid()
+    {
+        assertFalse(array.isValidIndex(negativeIndex))
+        assertFalse(array.isValidIndex(outOfBoundsIndex))
+    }
+
+    @Test
+    fun isValidIndexIn()
+    {
+        assertTrue(0.isValidIndexIn(array))
+        assertTrue(goodIndex.isValidIndexIn(array))
+    }
+
+    @Test
+    fun isValidIndexInWhenInvalid()
+    {
+        assertFalse(negativeIndex.isValidIndexIn(array))
+        assertFalse(outOfBoundsIndex.isValidIndexIn(array))
+    }
+
+    @Test
+    fun testAnyElement()
+    {
+        val element = array.anyElement
+
+        assertThat(element, notNull)
+        assertThat(element!!, isIn(array.toList()))
+    }
+
+    @Test
+    fun testAnyElementWhenEmpty()
+    {
+        val emptyList = listOf<String>()
+        val element = emptyList.anyElement
+        assertThat(element, isNull)
+    }
+
+}
