56787d44bb5123156727c2c3b39fbd906894837d
Wellington Moreno <wellington@censio.love>
2017-11-29 21:15:04 -0800

    Adds List extension functions

---
+++ src/main/java/tech/sirwellington/alchemy/kotlin/extensions/ListsPlus.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2017 SirWellington Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.sirwellington.alchemy.kotlin.extensions
+
+import java.util.Collections
+
+
+/**
+ *
+ * @author SirWellington
+ */
+
+/**
+ * Checks whether [index] is a valid index in `this` [List].
+ *
+ * @return `true` if [index] is a valid index in `this`, `false` otherwise.
+ */
+fun <T> List<T>.isValidIndex(index: Int): Boolean
+{
+    if (index < 0) return false
+
+    return index < size
+}
+
+/**
+ * Checks to make sure [index] is not a valid index in this [List].
+ *
+ * @return `true` if [index] is an invalid index in `this`, `false` if it a valid index.
+ */
+fun <T> List<T>.notValidIndex(index: Int) = !isValidIndex(index)
+
+/**
+ * @return `true` if `this` [Int] is a valid index in [list], false if it is not.
+ */
+fun <T> Int.isValidIndexIn(list: List<T>): Boolean
+{
+    if (this < 0) return false
+
+    return this < list.size
+}
+
+/**
+ * @return true if this [Number][Int] is a valid index in the given [List],
+ * false otherwise.
+ */
+fun <T> Int.notValidIndexIn(list: List<T>) = !isValidIndexIn(list)
+
+/**
+ * @return A shuffled version of this [List].
+ */
+fun <T> List<T>.shuffled(): List<T>
+{
+    val result = this.toMutableList()
+    Collections.shuffle(result)
+    return result
+}
+
+/**
+ *
+ * @return a random element from the list, null if the list is empty or null.
+ */
+val <T> List<T>?.anyElement: T?
+    get()
+    {
+        if (this == null) return null
+
+        val index = (0..size).random()
+
+        return if (index.isValidIndexIn(this)) this[index] else null
+    }
+
+/**
+ * Removes all elements from the collection that match the given predicate.
+ */
+fun <T> MutableCollection<T>.removeElementIf(predicate: (T) -> (Boolean))
+{
+    val elementsToRemove = this.filter(predicate).toList()
+    this.removeAll(elementsToRemove)
+}
+
+/**
+ * Adds an element to be front of a [List].
+ */
+inline fun <reified T> MutableList<T>.addToFront(element: T)
+{
+    add(0, element)
+}
---
+++ src/test/java/tech/sirwellington/alchemy/kotlin/extensions/ListsPlusKtTest.kt
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2017 SirWellington Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.sirwellington.alchemy.kotlin.extensions
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import tech.sirwellington.alchemy.generator.CollectionGenerators.Companion.listOf
+import tech.sirwellington.alchemy.generator.NumberGenerators.Companion.integers
+import tech.sirwellington.alchemy.generator.NumberGenerators.Companion.negativeIntegers
+import tech.sirwellington.alchemy.generator.StringGenerators
+import tech.sirwellington.alchemy.generator.StringGenerators.Companion.strings
+import tech.sirwellington.alchemy.generator.one
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner
+import tech.sirwellington.alchemy.test.junit.runners.Repeat
+
+@RunWith(AlchemyTestRunner::class)
+@Repeat
+class ListsPlusKtTest
+{
+
+    private lateinit var list: List<String>
+
+    private lateinit var newElement: String
+
+    @Before
+    fun setup()
+    {
+        list = listOf(strings())
+        newElement = StringGenerators.strings().get()
+    }
+
+    @Test
+    fun testIsValidIndexWhenInvalid()
+    {
+        val negativeIndex = one(negativeIntegers())
+        val outOfBoundsIndex = one(integers(list.size, Int.MAX_VALUE))
+
+        assertFalse(list.isValidIndex(negativeIndex))
+        assertFalse(list.isValidIndex(outOfBoundsIndex))
+    }
+
+    @Test
+    fun testIsValidIndexWhenValid()
+    {
+        val goodIndex = one(integers(0, list.size))
+        assertTrue(list.isValidIndex(goodIndex))
+        assertTrue(list.isValidIndex(0))
+    }
+
+    @Test
+    fun testIsValidIndexInWhenValid()
+    {
+        val goodIndex = one(integers(0, list.size))
+        assertTrue(goodIndex.isValidIndexIn(list))
+        assertTrue(0.isValidIndexIn(list))
+    }
+
+    @Test
+    fun testIsValidIndexInWhenInvalid()
+    {
+        val badIndex1 = one(negativeIntegers())
+        val badIndex2 = one(integers(list.size, Int.MAX_VALUE))
+
+        assertFalse(badIndex1.isValidIndexIn(list))
+        assertFalse(badIndex2.isValidIndexIn(list))
+    }
+
+    @Test
+    fun testAddToFront()
+    {
+        val mutableList = list.toMutableList()
+        mutableList.addToFront(newElement)
+
+        val expected = listOf(newElement) + list
+
+        assertThat(mutableList, equalTo(expected))
+    }
+
+    @Test
+    fun testAddToFrontWhenListEmpty()
+    {
+        val emptyList = mutableListOf<String>()
+        emptyList.addToFront(newElement)
+
+        val expected = mutableListOf(newElement)
+
+        assertThat(emptyList, equalTo(expected))
+    }
+}
