d7c11c2bfcc3c38595144d97d5bafe861bdfa082
Wellington Moreno <wellington@censio.love>
2017-11-29 21:34:35 -0800

    Adds extensions to all object references

---
+++ src/main/java/tech/sirwellington/alchemy/kotlin/extensions/ReferencesPlus.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2017 SirWellington Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.sirwellington.alchemy.kotlin.extensions
+
+import java.lang.ref.WeakReference
+
+
+/**
+ *
+ * @author SirWellington
+ */
+
+/**
+ * Creates a Weak Reference to [ref], and returns a closure that, when called, passes it to [block].
+ *
+ * @param [ref] The object to turn into a weak reference
+ * @param [block] The closure to call with the weak reference.
+ *
+ * @return A closure that, when invoked, calls [block] with a [weak reference][WeakReference] to [ref].
+ */
+inline fun <reified T> weak(ref: T, crossinline block: (ref: WeakReference<T>) -> Unit): () -> Unit
+{
+    val weakRef = ref.asWeak()
+
+    return {
+        block(weakRef)
+    }
+}
+
+/**
+ * Similar to [weak], except that the closure [block] is only called if
+ * [ref] hasn't been garbage-collected, and unwraps to a non-null reference.
+ */
+inline fun <reified T> weakNonNull(ref: T, crossinline block: (ref: T) -> Unit): (T) -> Unit
+{
+    val weakReference = ref.asWeak()
+
+    return function@ {
+        val strongReference = weakReference.get() ?: return@function
+        block(strongReference)
+    }
+}
+
+/**
+ * @return A [weak reference][WeakReference] to `this`.
+ */
+fun <T> T.asWeak(): WeakReference<T> = WeakReference(this)
+
+/**
+ * @return `true` if `this` [WeakReference] points to the same object as [other].
+ */
+fun <T> WeakReference<T>.isTheSameReferenceAs(other: WeakReference<T>): Boolean
+{
+    return this.get() === other.get()
+}
+
+/**
+ * @return `true` if this [List] of [WeakReference][WeakReference] already contains a reference to [weakReference],
+ *         `false` otherwise.
+ */
+fun <T> List<WeakReference<T>>.alreadyContainsWeakRef(weakReference: WeakReference<T>): Boolean
+{
+    forEach {
+        if (it.isTheSameReferenceAs(weakReference))
+        {
+            return true
+        }
+    }
+
+    return false
+}
---
+++ src/test/java/tech/sirwellington/alchemy/kotlin/extensions/ReferencesPlusTest.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2017 SirWellington Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.sirwellington.alchemy.kotlin.extensions
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.sameInstance
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import tech.sirwellington.alchemy.generator.StringGenerators
+import tech.sirwellington.alchemy.generator.one
+import tech.sirwellington.alchemy.test.hamcrest.isNull
+import tech.sirwellington.alchemy.test.hamcrest.notNull
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner
+
+@RunWith(AlchemyTestRunner::class)
+class ReferencesPlusTest
+{
+
+    private lateinit var originalReference: String
+
+    @Before
+    fun setUp()
+    {
+        originalReference = one(StringGenerators.alphabeticStrings())
+    }
+
+    @Test
+    fun testAsWeak()
+    {
+        val weakReference = originalReference.asWeak()
+
+        assertThat(weakReference, notNull)
+        assertThat(weakReference.get(), notNull)
+        assertThat(weakReference.get(), equalTo(originalReference))
+    }
+
+    @Test
+    fun testAsWeakWhenCollected()
+    {
+        val weakReference = originalReference.asWeak()
+        originalReference = one(StringGenerators.alphabeticStrings())
+
+        System.gc()
+
+        assertThat(weakReference, notNull)
+        assertThat(weakReference.get(), isNull)
+
+    }
+
+    @Test
+    fun testIsTheSameReferenceAs()
+    {
+        val first = originalReference.asWeak()
+        val second = originalReference.asWeak()
+        assertThat(first, !sameInstance(second))
+    }
+
+    @Test
+    fun testAlreadyContainsWeakRef()
+    {
+        val first = originalReference.asWeak()
+        val list = mutableListOf(first)
+        val second = originalReference.asWeak()
+
+        assertThat(first, !equalTo(second))
+        assertTrue(list.alreadyContainsWeakRef(second))
+    }
+
+    @Test
+    fun testAlreadyContainsWeakRefWhenItDoesNot()
+    {
+        val firstRef = originalReference.asWeak()
+        val secondValue = one(StringGenerators.alphabeticStrings())
+        val secondRef = secondValue.asWeak()
+
+        val list = mutableListOf(firstRef)
+
+        assertTrue(list.alreadyContainsWeakRef(originalReference.asWeak()))
+        assertFalse(list.alreadyContainsWeakRef(secondRef))
+
+        list.add(secondRef)
+
+        assertThat(list.size, equalTo(2))
+    }
+
+}
